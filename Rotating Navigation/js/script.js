e23d03a27564aa4ce00bc1cd12903b82a6ed4c3e
Abdellah Slimani <85078022+Ci10024@users.noreply.github.com>
2021-11-29 16:15:39 +0100

    Add files via upload

---
+++ Rotating Navigation/js/script.js
@@ -0,0 +1,8 @@
+const container = document.querySelector('.container');
+
+function openNav() {
+    container.classList.add('show-nav');
+}
+function closeNav() {
+    container.classList.remove('show-nav');
+}
